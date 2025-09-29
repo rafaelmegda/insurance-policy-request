dd017300bc149a868fb68b2e576ca9eda5ea0924
rafaelmegda <rafaelmisidoro@gmail.com>
2025-09-29 14:04:16 -0300

    Create project springboot

---
+++ src/main/java/com/company/insurance_request/InsuranceRequestApplication.java
@@ -0,0 +1,13 @@
+package com.company.insurance_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/insurance_request/InsuranceRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.insurance_request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
