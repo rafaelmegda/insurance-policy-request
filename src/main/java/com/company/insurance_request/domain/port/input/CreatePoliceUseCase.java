5f2dc6371d79e6cd14f526724c260e1b184566af
rafaelmegda <rafaelmisidoro@gmail.com>
2025-10-02 18:24:32 -0300

    Refactor arch hexagonal

--- src/main/java/com/company/insurance_request/domain/port/input/CreatePoliceUseCase.java
+++ src/main/java/com/company/insurance_request/domain/port/input/CreatePoliceUseCase.java
@@ -1,8 +1,9 @@
 package com.company.insurance_request.domain.port.input;
 
 import com.company.insurance_request.domain.model.Policy;
-import com.company.insurance_request.infrastructure.adapter.input.dto.CreatePoliceRequest;
+import com.company.insurance_request.infrastructure.adapter.input.dto.PolicyRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface CreatePoliceUseCase {
-    Policy create(CreatePoliceRequest createPoliceRequest);
+    Policy create(PolicyRequest policyRequest) throws JsonProcessingException;
 }
